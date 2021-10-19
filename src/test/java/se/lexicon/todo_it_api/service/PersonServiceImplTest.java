68157c96ebf36fc16757ba07131b650c99e1419e
leoodindealcantara <leoalcantara@icloud.com>
2021-10-19 10:28:18 +0200

    Sixth Commit

---
+++ src/test/java/se/lexicon/todo_it_api/service/PersonServiceImplTest.java
@@ -0,0 +1,45 @@
+package se.lexicon.todo_it_api.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findIdlePeople() {
+    }
+
+    @Test
+    void addTodoItem() {
+    }
+
+    @Test
+    void removeTodoItem() {
+    }
+
+    @Test
+    void update() {
+    }
+}
---
+++ src/test/java/se/lexicon/todo_it_api/service/TodoItemServiceImplTest.java
@@ -0,0 +1,65 @@
+package se.lexicon.todo_it_api.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoItemServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findAllByPersonId() {
+    }
+
+    @Test
+    void findAllUnassigned() {
+    }
+
+    @Test
+    void findAllUnfinishedAndOverdue() {
+    }
+
+    @Test
+    void findByDeadlineAfter() {
+    }
+
+    @Test
+    void findByDeadlineBefore() {
+    }
+
+    @Test
+    void findByDeadlineBetween() {
+    }
+
+    @Test
+    void findByDoneStatus() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByTitle() {
+    }
+
+    @Test
+    void update() {
+    }
+}
